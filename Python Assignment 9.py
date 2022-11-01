ce6de1dc6b6782910a87cb9eac881bba260c7e56
Arshad Shaikh <113104191+ShaikhArshad11@users.noreply.github.com>
2022-11-01 22:01:23 +0530

    Add files via upload

---
+++ Python Assignment 9.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. To what does a relative path refer?
+ans : Relative path is the path related to the present working directly(pwd).2. What does an absolute path start with your operating system?
+ans : An absolute path is a complete path from start of actual file system from  directory.3. What do the functions os.getcwd() and os.chdir() do?
+ans : os.getcwd() method tells us the location of current working directory (CWD).
+      os.chdir() method is used to change the current working directory to specified path. 4. What are the . and .. folders?5. In C:\bacon\eggs\spam.txt, which part is the dir name, and which part is the base name?
+ans : C:\bacon\eggs is the dir name and spam.txt is the base name .6. What are the three “mode” arguments that can be passed to the open() function?
+ans : r : opens the file in read only mode.
+     r+ : opens a file for reading and writing purpose .
+     rb : Opens the file as read-only in binary format .7. What happens if an existing file is opened in write mode?
+ans : The file will be treated as a new empty file.8. How do you tell the difference between read() and readlines()?
+ans : read() method returns the specified number of bytes from the file. 
+      readlines() method only reads the single line of a file .9. What data structure does a shelf value resemble?
+ans : Dictionary value.
